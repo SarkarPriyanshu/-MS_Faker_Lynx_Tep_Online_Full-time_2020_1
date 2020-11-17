39e578ca3403e147a9fa0c2f5682ad3e8ef9d892
Spawar1530 <74126991+Spawar1530@users.noreply.github.com>
2020-11-17 11:43:35 +0530

    Update random.js

--- random.js
+++ random.js
@@ -1,5 +1,25 @@
 var faker = require('faker');
 
-var randomName = faker.name.findName(); 
-var randomEmail = faker.internet.email(); 
-var randomCard = faker.helpers.createCard();
+var firstName = faker.name.firstName();
+var lastName = faker.name.lastName();
+
+var jobTitle = faker.name.jobTitle();
+var prefix = faker.name.prefix(); 
+var suffix = faker.name.suffix();
+var jobArea = faker.name.jobArea();
+
+var phone = faker.phone.phoneNumber();
+
+var futureDate = faker.date.future();
+var recentDate = faker.date.recent();
+var weekday = faker.date.weekday();
+
+console.log(`Employee: ${prefix} ${firstName} ${lastName} ${suffix}`);
+console.log(`Job title: ${jobTitle}`);
+console.log(`Job area: ${jobArea}`);
+console.log(`Phone: ${phone}`);
+console.log(futureDate);
+console.log(recentDate);
+console.log(weekday);
+
+
